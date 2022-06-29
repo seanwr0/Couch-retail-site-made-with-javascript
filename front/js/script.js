612c9a0da074c7c042d242e7df826754efd6e5c4
sean <srosen6136@mpc.edu>
2022-06-28 17:09:56 -0700

    added the products to the main page

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,15 @@
+const productSection = document.getElementById('items');
 
+let productDetails = [];
+
+let image = [];
+let imageAlt = [];
+
+let newProduct = [];
+let newImg = [];
+let newName = [];
+let newDescription = [];
+let newLink = [];
 
 
 // fuction that gets the product info from the server and 
@@ -25,3 +36,42 @@ async function getProductWithPromise() {
     });
 }
 
+
+
+// calls the getProductWithPromise function then use that to create the products and add them to the page
+getProductWithPromise().then((productInfo) => {
+
+    productDetails = productInfo;
+
+
+
+    for (let i = 0; i < productInfo.length; i++) {
+
+        image[i] = productDetails[i].imageUrl;
+        imageAlt[i] = productDetails[i].altTxt;
+
+        newLink[i] = document.createElement('a');
+        newProduct[i] = document.createElement('article');
+        newImg[i] = document.createElement('img');
+        newName[i] = document.createElement('h3');
+        newDescription[i] = document.createElement('p');
+
+        newLink[i].href = "./product.html?id=42";
+        newImg[i].src = image[i];
+        newImg[i].alt = imageAlt[i];
+
+        newName[i].textContent = productDetails[i].name;
+        newDescription[i].textContent = productDetails[i].description;
+
+        newProduct[i].appendChild(newImg[i]);
+        newProduct[i].appendChild(newName[i]);
+        newProduct[i].appendChild(newDescription[i]);
+        
+        newLink[i].appendChild(newProduct[i])
+
+        productSection.appendChild(newLink[i]);
+
+        newName[i].classList.add('productName');
+        newDescription[i].classList.add('productDescription');
+    }
+})
